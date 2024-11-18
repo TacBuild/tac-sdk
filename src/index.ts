a18732a73af7b4e75bc57d07c110c0703df2e777
vitalii <vitalii@fanz.ee>
2024-11-18 18:19:12 +0300

    update package.json

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export type { JettonWalletData } from './ton/jetton/JettonWallet';
 export { TacSdk } from './ton/sdk/TacSdk';
 export type { TacSDKTonClientParams, JettonTransferData, EvmProxyMsg, TransactionLinker, TransferMessage, ShardTransaction } from './ton/structs/Struct';
 export { Network, SimplifiedStatuses, OpCode } from './ton/structs/Struct';
+
+export { TonConnectSender, RawSender } from './ton/sender_abstraction/SenderAbstraction';
