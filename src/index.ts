f1c49e4505f338593f168f2105ad2a5f43a6ce2f
Sergey Peregudov <serega55555q88888@mail.ru>
2024-11-18 17:16:07 +0300

    extend index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { JettonWallet, JettonWalletOpCodes } from './ton/jetton/JettonWallet';
 export type { JettonWalletData } from './ton/jetton/JettonWallet';
 export { TacSdk } from './ton/sdk/TacSdk';
-export type { TacSDKTonClientParams, JettonTransferData } from './ton/structs/Struct';
+export type { TacSDKTonClientParams, JettonTransferData, EvmProxyMsg, TransactionLinker, TransferMessage, ShardTransaction } from './ton/structs/Struct';
+export { Network, SimplifiedStatuses, OpCode } from './ton/structs/Struct';
