95f233eb100b4f055a70234a9b73d284f057b804
Danila Moriakov <dm@tonappchain.fi>
2024-12-20 16:54:35 +0300

    rm redundant

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/l1_tvm_ton/'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
     moduleNameMapper: {
         '^axios$': require.resolve('axios'),
     },
