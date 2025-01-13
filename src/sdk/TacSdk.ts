0d09a38ee4f049cd3f9a4c1734995eab09cc1eb5
GavrilenkoDS <dimatygavrilenko@gmail.com>
2025-01-13 17:22:08 +0300

    fix artifacts

--- src/sdk/TacSdk.ts
+++ src/sdk/TacSdk.ts
@@ -64,6 +64,8 @@ export class TacSdk {
         this.TACParams = tacParams;
         this.network = network;
         this.delay = delay;
+        this.artifacts = this.network === Network.Testnet ? testnet : mainnet;
+
     }
 
     static async createSDK(sdkParams: SDKParams): Promise<TacSdk> {
