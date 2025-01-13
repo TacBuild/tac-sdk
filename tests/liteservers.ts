0e5e78a5203468e6b9a98ea83286b5959834a254
Danila Moriakov <dm@tonappchain.fi>
2025-01-13 22:17:02 +0300

    use own ton lite clients

--- tests/liteservers.ts
+++ tests/liteservers.ts
@@ -3,17 +3,18 @@ import { Network, TacSdk, liteClientOpener } from '../src';
 const NETWORK = Network.Testnet;
 
 async function main() {
+    const contractOpener = await liteClientOpener({ network: NETWORK });
     const sdk = await TacSdk.create({
         network: Network.Testnet,
         delay: 0,
         TONParams: {
-            contractOpener: await liteClientOpener({ network: NETWORK }),
-        }
+            contractOpener,
+        },
     });
 
     console.log(await sdk.getEVMTokenAddress('EQCsQSo54ajAorOfDUAM-RPdDJgs0obqyrNSEtvbjB7hh2oK'));
 
-    process.exit(); // TODO: ton-lite-client lib does not stop some of its tasks, so process does not stop by itself. [https://github.com/ton-core/ton-lite-client/issues/10]
+    contractOpener.closeConnections();
 }
 
 main();
