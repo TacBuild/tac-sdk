de5be10f1928436d05280fc142aabc67a9e23a1d
Danila Moriakov <dm@tonappchain.fi>
2025-05-16 07:06:09 +0300

    dynamic chunk size in tonconnect

--- src/sender/TonConnectSender.ts
+++ src/sender/TonConnectSender.ts
@@ -7,7 +7,7 @@ import { ContractOpener, Network } from '../structs/Struct';
 import { SenderAbstraction, sleep } from './SenderAbstraction';
 import { SendTransactionResponse } from '@tonconnect/sdk';
 
-const CHUNK_SIZE = 4; // TODO: how to detect that tonConnect connected to W5 and CHUNK_SIZE is 254?
+const CHUNK_SIZE = 4;
 
 export class TonConnectSender implements SenderAbstraction {
     readonly tonConnect: TonConnectUI;
@@ -16,32 +16,46 @@ export class TonConnectSender implements SenderAbstraction {
         this.tonConnect = tonConnect;
     }
 
-    private async sendChunkedMessages(messages: SendTransactionRequest['messages'], validUntil: number, chain: Network): Promise<SendTransactionResponse[]> {
+    private async sendChunkedMessages(
+        messages: SendTransactionRequest['messages'],
+        validUntil: number,
+        chain: Network,
+    ): Promise<SendTransactionResponse[]> {
         const responses: SendTransactionResponse[] = [];
-        
-        for (let i = 0; i < messages.length; i += CHUNK_SIZE) {
-            const chunk = messages.slice(i, i + CHUNK_SIZE);
+
+        const chunkSize =
+            //@ts-ignore // 'find' checks that maxMessages is a property of the feature
+            this.tonConnect.wallet?.device.features.find((feat) => feat.hasOwnProperty('maxMessages'))?.maxMessages ||
+            CHUNK_SIZE;
+
+        for (let i = 0; i < messages.length; i += chunkSize) {
+            const chunk = messages.slice(i, i + chunkSize);
             const transaction: SendTransactionRequest = {
                 validUntil,
                 messages: chunk,
                 network: chain == Network.TESTNET ? CHAIN.TESTNET : CHAIN.MAINNET,
             };
-            
+
             const response = await this.tonConnect.sendTransaction(transaction);
             responses.push(response);
-            
-            if (i + CHUNK_SIZE < messages.length) {
+
+            if (i + chunkSize < messages.length) {
                 await sleep(1000);
             }
         }
-        
+
         return responses;
     }
 
-    async sendShardTransactions(shardTransactions: ShardTransaction[], delay: number, chain: Network, contractOpener?: ContractOpener): Promise<SendTransactionResponse[]> {
+    async sendShardTransactions(
+        shardTransactions: ShardTransaction[],
+        delay: number,
+        chain: Network,
+        contractOpener?: ContractOpener,
+    ): Promise<SendTransactionResponse[]> {
         const allMessages = [];
         let minValidUntil = 0;
-        
+
         for (const transaction of shardTransactions) {
             for (const message of transaction.messages) {
                 allMessages.push({
