19b4e8b989bfee89da0bc114244fe37e5accc865
Aleksei Kokinos <akkokinos@gmail.com>
2025-09-23 01:47:39 +0700

    fix tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/artifacts/'],
     moduleNameMapper: {
         '^axios$': require.resolve('axios'),
     },
